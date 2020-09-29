02d3dadee84f298ce64412a42b4842d3404e2ca9
ATMECS\mohammed.rizwan <mohammed.rizwan@atmecs.com>
2020-09-29 20:25:11 +0530

    commit

---
+++ constants.java
@@ -0,0 +1,11 @@
+package com.atmecs.dataexcel.constants;
+
+public class constants {
+
+		public static final String BASE_PATH = System.getProperty("user.dir");
+		public static final String RESOURCE_PATH = BASE_PATH + "/src/main/resources";
+		public static final String TESTDATA_PATH = RESOURCE_PATH + "/dataFile.xlsx";
+		public static final String LOCATORS_PATH = RESOURCE_PATH + "/phpTravelsLocators.properties";
+		public static final String CHROME_PATH = BASE_PATH + "/lib/chromedriver_win32/chromedriver.exe";
+
+	}
